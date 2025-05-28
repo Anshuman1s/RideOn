1ca27c4f611b2e934eca4d277e697707e48ae1de
Anshuman <anshumanshukla.12as@gmail.com>
2025-05-28 10:12:17 +0530

    Register route

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,11 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
+const userRoutes = require('./route/user.routes')
 connectToDb();
 app.use(cors());
+app.use(express.json());
+app.use('/users',userRoutes)
 app.get('/',(req,res)=>{
     res.send('Hey Anshuman')
 })
