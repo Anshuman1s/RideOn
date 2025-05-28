5aef409a89139345461ba05d7dc8f889187de1e3
Anshuman <anshumanshukla.12as@gmail.com>
2025-05-28 06:12:02 +0530

    Controllers Model Services route

---
+++ Backend/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectToDb = async () => {
+    try {
+        await mongoose.connect(process.env.DB_CONNECT, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        }).then(() => {
+            console.log('MongoDB connection established');
+        });
+        
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
+    }
+module.exports = connectToDb;
